4c971536da0e24b7735d7b76276abc5a98612b35
jhonndabi <jhonndabi.s.n@hotmail.com>
2018-06-04 20:11:09 -0300

    add guzzle exception handle

--- app/Services/AbstractService.php
+++ app/Services/AbstractService.php
@@ -5,12 +5,17 @@ namespace App\Services;
 use GuzzleHttp\Client as HttpClient;
 use GuzzleHttp\Psr7\Response;
 use GuzzleHttp\RequestOptions;
+use GuzzleHttp\Exception\ClientException;
 
 abstract class AbstractService
 {
     const DATABASE_API_VERSION = '/api/v1';
     const DATABASE_API_EMAIL = 'team@stark.com';
     const DATABASE_API_TOKEN = 'dfJ2wsUU9kwSRnNk5aww';
+    const NOT_FOUND = 404;
+    const NOT_FOUND_MESSAGE = 'Not found.';
+    const INTERNAL_SERVER_ERROR = 500;
+    const INTERNAL_SERVER_ERROR_MESSAGE = 'Internal server error.';
 
     /**
      * @see http://docs.guzzlephp.org/en/stable/
@@ -48,7 +53,12 @@ abstract class AbstractService
     public function find($id)
     {
         $options = $this->defaultOptions();
-        $response = $this->httpClient->get($this->databaseApiUrl("/{$this->resource}/{$id}"), $options);
+
+        try {
+            $response = $this->httpClient->get($this->databaseApiUrl("/{$this->resource}/{$id}"), $options);
+        } catch (ClientException $e) {
+            $this->handleGuzzleException($e);
+        }
 
         return $this->parseResponseToJson($response);
     }
@@ -56,7 +66,12 @@ abstract class AbstractService
     public function findAll()
     {
         $options = $this->defaultOptions();
-        $response = $this->httpClient->get($this->databaseApiUrl("/{$this->resource}"), $options);
+
+        try {
+            $response = $this->httpClient->get($this->databaseApiUrl("/{$this->resource}"), $options);
+        } catch (ClientException $e) {
+            $this->handleGuzzleException($e);
+        }
 
         return $this->parseResponseToJson($response);
     }
@@ -67,7 +82,12 @@ abstract class AbstractService
         $options = $this->defaultOptions();
         $options = array_merge_recursive($options, $body);
 
-        $response = $this->httpClient->post($this->databaseApiUrl("/{$this->resource}"), $options);
+        try {
+            $response = $this->httpClient->post($this->databaseApiUrl("/{$this->resource}"), $options);
+        } catch (ClientException $e) {
+            $this->handleGuzzleException($e);
+        }
+
         return $this->parseResponseToJson($response);
     }
 
@@ -77,16 +97,38 @@ abstract class AbstractService
         $options = $this->defaultOptions();
         $options = array_merge_recursive($options, $body);
 
-        $response = $this->httpClient->put($this->databaseApiUrl("/{$this->resource}/{$id}"), $options);
+        try {
+            $response = $this->httpClient->put($this->databaseApiUrl("/{$this->resource}/{$id}"), $options);
+        } catch (ClientException $e) {
+            $this->handleGuzzleException($e);
+        }
+
         return $this->parseResponseToJson($response);
     }
 
     public function delete(int $id)
     {
         $options = $this->defaultOptions();
-        $response = $this->httpClient->delete($this->databaseApiUrl("/{$this->resource}/{$id}"), $options);
+
+        try {
+            $response = $this->httpClient->delete($this->databaseApiUrl("/{$this->resource}/{$id}"), $options);
+        } catch (ClientException $e) {
+            $this->handleGuzzleException($e);
+        }
 
         return $this->parseResponseToJson($response);
+    }
+
+    public function handleGuzzleException(ClientException $e)
+    {
+        switch ($e->getCode()) {
+            case self::NOT_FOUND:
+                abort(self::NOT_FOUND, self::NOT_FOUND_MESSAGE);
+                break;
+            default:
+                abort(self::INTERNAL_SERVER_ERROR, self::INTERNAL_SERVER_ERROR_MESSAGE);
+                break;
+        }
     }
 
     private function parseResponseToJson(Response $response)
